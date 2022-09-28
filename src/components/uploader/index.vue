e19df77b3b3a8f8bb14b1874359bf253f53ec5a3
Realabiha <m17386550687@163.com>
2022-09-28 14:48:25 +0800

    update: 添加uploader组件

---
+++ src/components/uploader/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-upload
+        class="upload-demo"
+        :show-file-list="false"
+        action=""
+        multiple
+        :limit="3"
+        :file-list="fileList"
+        :on-change="onFileChange"
+        :http-request="httpRequest"
+    >
+    <el-button type="primary">点击上传</el-button>
+    <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div> -->
+    </el-upload>
+</template>
+<script>
+  export default {
+    name: 'Uploader',
+    data() {
+      return {
+        fileList: [],
+        fd: new FormData()
+      };
+    },
+    methods: {
+        onFileChange(_, fileList){
+            this.fileList = fileList
+        },
+        httpRequest({file}){
+            this.fd.append('files', file)
+            console.log(this.fd.getAll('files'))
+        }
+    }
+  }
+</script>
